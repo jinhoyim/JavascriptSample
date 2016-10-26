bb20a75cc8dd4d07bea92182c89e4d8da837c746
Jinho Yim <jinhoyim@outlook.com>
2016-10-26 22:28:23 +0900

    closure 테스트

---
+++ Closure-First-Test/closure.js
@@ -0,0 +1,54 @@
+(function(global){
+    var doc = global.document;
+    var buttons = doc.querySelectorAll('.buttons button');
+    var btn_apply = doc.querySelector('.btn-apply');
+    var btn_start = doc.querySelector('.btn-start');
+
+    var apple_stock = doc.querySelector('#apple_stock');
+    var orange_stock = doc.querySelector('#orange_stock');
+    var banana_stock = doc.querySelector('#banana_stock');
+
+    var isStart = false;
+
+    var getOrderFruitService = function(fruit_name, fruit_count){
+        var orderFruit = function(){
+            fruit_count--;
+            var view = doc.querySelector('.' + fruit_name);
+            view.innerHTML = fruit_count;
+        }
+        return orderFruit;
+    };
+
+    var fruits = {
+        'apple': 0,
+        'orange': 0,
+        'banana': 0
+    }
+    global.fruits = fruits;
+
+    btn_apply.onclick = function(){
+        fruits.apple = apple_stock.value;
+        fruits.orange = orange_stock.value;
+        fruits.banana = banana_stock.value;
+        
+        if(!isStart)
+        {
+            for(var prop in fruits)
+            {
+                var view = doc.querySelector('.' + prop);
+                view.innerHTML = fruits[prop];
+            }
+            isStart = true;
+        }
+    };
+
+    btn_start.onclick = function(){
+        for(var i = 0, l = buttons.length; i < l; i++)
+        {
+            fruit_name = buttons[i].getAttribute('data-fruit');
+            buttons[i].onclick = getOrderFruitService(fruit_name, fruits[fruit_name]);
+        }
+    }
+
+    
+})(this);
